dfa4e4df2953bd77f4de350e4056052e4d189e77
pghezzi <100241979+pghezzi@users.noreply.github.com>
2023-10-16 15:25:26 -0400

    Update HomePage.js

--- frontend/docuview-react-app/src/HomePage.js
+++ frontend/docuview-react-app/src/HomePage.js
@@ -3,18 +3,37 @@ import Header from "./atoms/Header.jsx";
 import Button from "./atoms/Button.jsx";
 import { Tab } from "@headlessui/react";
 
+// COMPONENTS TO MOVE //
+
+// Buttons can do things, but where are you going to get the data from ?
 function HomePageBotton({ text, OnClick }){
   return (
-      <Button width="w-32" height="h-10" OnClick={() => OnClick}>
-        {" "}
-        { text }{" "}
-      </Button>
+      <div className="flex justify-end">
+        <Button width="w-32" height="h-10" OnClick={() => OnClick}>
+          {" "}
+          { text }{" "}
+        </Button>
+      </div>
   )
 }
 
+// Moved it for clarity (used 4 times in the single part)
+function HomePageInput( { label } ){
+  return (
+    <div><label> {label} </label><input className="h-10 w-full px-5 bg-iso-grey rounded-md" /></div>
+  )
+}
+
+//////////////////////
+
+
+// Forgot Username and Forgot Password do nothing ?
+
+// Do what you will with this, idk what exactly you want to do
+
 function HomePageTabs(){
   return (
-    <div>
+    <div className="flex h-full w-1/3 justify-center items-center">
       <Tab.Group>
         <Tab.List>
           <Tab className={`rounded-t-md ui-selected:bg-iso-white ui-not-selected:bg-iso-grey h-10 w-32`} >
@@ -26,32 +45,30 @@ function HomePageTabs(){
         </Tab.List> 
         <Tab.Panels>
           <Tab.Panel className="w-96 h-1/3 bg-iso-white p-10 rounded-b-md rounded-tr-md">
-            <label> Username </label>
-            <input className="h-10 w-full px-5 bg-iso-grey rounded-md" />
+            <HomePageInput label="Username" />
             <div className="flex w-full h-10 justify-end">
               {" "}
               <span> Forgot Username </span>{" "}
-            </div><label> Password </label><input className="h-10 w-full px-5 bg-iso-grey rounded-md" /><div className="flex w-full h-10 justify-end">
+            </div>
+            <HomePageInput label="Password" />
+            <div className="flex w-full h-10 justify-end">
               {" "}
               <span> Forgot Password </span>{" "}
             </div>
-            <div className="flex justify-end">
-              <HomePageBotton text="Login" />
-            </div>
+            <HomePageBotton text="Login" />
           </Tab.Panel>
           <Tab.Panel className="w-96 h-1/3 bg-iso-white p-10 rounded-b-md rounded-tr-md">
-            <label> Username </label>
-            <input className="h-10 w-full px-5 bg-iso-grey rounded-md" />
+            <HomePageInput label="Username" />
             <div className="flex w-full h-10 justify-end">
               {" "}
               {" "}
-            </div><label> Password </label><input className="h-10 w-full px-5 bg-iso-grey rounded-md" /><div className="flex w-full h-10 justify-end">
+            </div>
+            <HomePageInput label="Password" />
+            <div className="flex w-full h-10 justify-end">
               {" "}
               {" "}
             </div>
-            <div className="flex justify-end">
-              <HomePageBotton text="Sign Up" />
-            </div>
+            <HomePageBotton text="Sign Up" />
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
@@ -69,9 +86,7 @@ function HomePage() {
     <div className="flex flex-wrap w-screen h-screen bg-iso-blue">
       <Header />
       <div className="h-full w-1/3" />
-      <div className="flex h-full w-1/3 justify-center items-center">
-          < HomePageTabs />
-      </div>
+      < HomePageTabs />
       <div className="h-full w-1/3" />
     </div>
   );
