9cbb38263c315edbdf39f384112a1091b221c881
Adam Trac <atrac@umass.edu>
2023-10-18 18:10:07 -0400

    prettyify

--- frontend/src/AuthPage.js
+++ frontend/src/AuthPage.js
@@ -4,57 +4,67 @@ import Button from "./atoms/Button.jsx";
 import { Tab } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
 
-function HomePageButton({ text, OnClick }){
+function HomePageButton({ text, OnClick }) {
   return (
-      <div className="flex justify-end py-1">
-        <Button width="w-32" height="h-10" OnClick={OnClick}>
-          { text }
-        </Button>
-      </div>
-  )
+    <div className="flex justify-end py-1">
+      <Button width="w-32" height="h-10" OnClick={OnClick}>
+        {text}
+      </Button>
+    </div>
+  );
 }
 
 // Moved it for clarity (used 4 times in the single part)
-function HomePageInput( { label, hidden=false, forgot=false, forgotText } ){
+function HomePageInput({ label, hidden = false, forgot = false, forgotText }) {
   return (
     <div className="flex flex-col w-full">
       <label> {label} </label>
-      <input type={hidden && "password"} className="h-10 w-full px-5 bg-iso-grey rounded-md"/>
-      {forgot && (<span className="h-5 text-right">{forgotText}</span>)}
+      <input
+        type={hidden && "password"}
+        className="h-10 w-full px-5 bg-iso-grey rounded-md"
+      />
+      {forgot && <span className="h-5 text-right">{forgotText}</span>}
     </div>
-  )
+  );
 }
 
-function HomePageTabs(){
+function HomePageTabs() {
   const navigate = useNavigate();
-  const movePage = () => navigate('/main');
+  const movePage = () => navigate("/main");
   return (
     <div className="flex h-full w-1/3 justify-center items-center">
       <div>
         <Tab.Group>
           <Tab.List>
-            {["Login", "Sign Up"].map((txt)=>(
-              <Tab className={"tab"}>
-                {txt}
-              </Tab>
+            {["Login", "Sign Up"].map((txt) => (
+              <Tab className={"tab"}>{txt}</Tab>
             ))}
-          </Tab.List> 
+          </Tab.List>
           <Tab.Panels>
             <Tab.Panel className="tab-body">
-              <HomePageInput label="Username" forgot={true} forgotText={"Forgot Username"}/>
-              <HomePageInput label="Password" hidden={true} forgot={true} forgotText={"Forgot Password"}/>
-              <HomePageButton text="Login" OnClick={movePage}/>
+              <HomePageInput
+                label="Username"
+                forgot={true}
+                forgotText={"Forgot Username"}
+              />
+              <HomePageInput
+                label="Password"
+                hidden={true}
+                forgot={true}
+                forgotText={"Forgot Password"}
+              />
+              <HomePageButton text="Login" OnClick={movePage} />
             </Tab.Panel>
             <Tab.Panel className="flex flex-col tab-body gap-5">
               <HomePageInput label="Username" />
-              <HomePageInput label="Password" hidden={true}/>
-              <HomePageButton text="Sign Up" OnClick={movePage}/>
+              <HomePageInput label="Password" hidden={true} />
+              <HomePageButton text="Sign Up" OnClick={movePage} />
             </Tab.Panel>
           </Tab.Panels>
         </Tab.Group>
-      </ div>
+      </div>
     </div>
-  )
+  );
 }
 
 function HomePage() {
@@ -71,7 +81,6 @@ function HomePage() {
       <div className="flex-grow"></div>
     </div>
   );
-  
 }
 
 export default HomePage;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {    
+    extend: {
       colors: {
-      'iso-blue': '#21303A',
-      'iso-white': '#FFFFFF',
-      'iso-blue-grey': {
-        100: '#4A5B63',
-        200: '#6E7B82'
+        "iso-blue": "#21303A",
+        "iso-white": "#FFFFFF",
+        "iso-blue-grey": {
+          100: "#4A5B63",
+          200: "#6E7B82",
+        },
+        "iso-grey": "#d9d9d9",
       },
-      'iso-grey': '#d9d9d9'
-      }
     },
   },
-  plugins: [
-    require('@headlessui/tailwindcss')
-  ],
-}
-
+  plugins: [require("@headlessui/tailwindcss")],
+};
