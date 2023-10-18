c3754f6b544659a8b9435dda5ac27027cee122ac
Pablo Ghezzi <pgsurf@kali.kali>
2023-10-17 22:26:11 -0400

    BROKEN DO NOT USE

--- server/src/main/java/FWD_Development/DocuView/controllers/FiltersV1.java
+++ server/src/main/java/FWD_Development/DocuView/controllers/FiltersV1.java
@@ -5,6 +5,8 @@ package FWD_Development.DocuView.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.HashMap;
 /* CUSTOM ADDED LIBS */
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,21 +45,38 @@ import org.springframework.dao.EmptyResultDataAccessException;
 @RequestMapping("/api/v1/filters")
 public class FiltersV1 {
     private ObjectMapper objMapper = new ObjectMapper();
-    private String[][] filtersArray = {
-        {"file_creation", "ISO 8601 (YYYY-MM-DD)"},
-        {"document_type", "string"},
-        {"file_name", "string"},
-        {"customer_name", "string"},
-        {"auction_type", "string"},
-        {"proposal_type", "string"},
-        {"project_type", "string"},
-        {"commitment_date_start", "ISO 8601 (YYYY-MM-DD)"},
-        {"commitment_date_end", "ISO 8601 (YYYY-MM-DD)"},
-        {"auction_date_start", "ISO 8601 (YYYY-MM-DD)"},
-        {"auction_date_end", "ISO 8601 (YYYY-MM-DD)"},
-        {"proposal_date_start", "ISO 8601 (YYYY-MM-DD)"},
-        {"proposals_date_end", "ISO 8601 (YYYY-MM-DD)"},
+    //usind MDA to be able to store filter and properties properly
+    private static final Map<String, String[][]> m = new HashMap<String, String[][]>() {{
+	    put("file_creation", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}, {}});
+	    put("document_type", new String[][] {{"string"}, {}});
+	    put("file_name", new String[][] {{"string"}, {}});
+	    put("customer_name", new String[][] {{"string"}, {}});
+	    put("auction_type", new String[][] {{"string"}, {}});
+	    put("proposal_type", new String[][] {{"string"}});
+	    put("project_type", new String[][] {{"string"}});
+	    put("commitment_date_start", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	    put("commitment_date_end", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	    put("auction_date_start", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	    put("auction_date_end", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	    put("proposal_date_start", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	    put("proposals_date_end", new String[][] {{"ISO 8601 (YYYY-MM-DD)"}});
+	}};
+    private String[][][] filtersArray = {
+        {{"file_creation"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"document_type"}, {"string"}},
+        {{"file_name"}, {"string"}},
+        {{"customer_name"}, {"string"}},
+        {{"auction_type"}, {"string"}},
+        {{"proposal_type"}, {"string"}},
+        {{"project_type"}, {"string"}},
+        {{"commitment_date_start"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"commitment_date_end"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"auction_date_start"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"auction_date_end"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"proposal_date_start"}, {"ISO 8601 (YYYY-MM-DD)"}},
+        {{"proposals_date_end"}, {"ISO 8601 (YYYY-MM-DD)"}},
     };
+
     
 
     @GetMapping("")
@@ -65,12 +84,9 @@ public class FiltersV1 {
         ObjectNode rootNode = objMapper.createObjectNode();
         ObjectNode filtersNode = objMapper.createObjectNode();
         rootNode.set("list", filtersNode);
-        for (String[] item : filtersArray){
-            filtersNode.put(item[0], item[1]);
+        for (Map.Entry<String, String[][]> set : m.entrySet()){
+            filtersNode.put(set.getKey(), set.getValue()[0][0]);
         }
-        
-
-        
         return rootNode;
     }
 }
