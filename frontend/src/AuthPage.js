359c74e101615c24e96cbe531ab5f048c8c97bd6
Adam Trac <atrac@umass.edu>
2023-10-18 18:09:02 -0400

    tailwind classes + refactor

--- frontend/src/AuthPage.js
+++ frontend/src/AuthPage.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 function HomePageButton({ text, OnClick }){
   return (
-      <div className="flex justify-end">
+      <div className="flex justify-end py-1">
         <Button width="w-32" height="h-10" OnClick={OnClick}>
           { text }
         </Button>
@@ -20,7 +20,7 @@ function HomePageInput( { label, hidden=false, forgot=false, forgotText } ){
     <div className="flex flex-col w-full">
       <label> {label} </label>
       <input type={hidden && "password"} className="h-10 w-full px-5 bg-iso-grey rounded-md"/>
-      {forgot && (<span className="h-10 text-right">{forgotText}</span>)}
+      {forgot && (<span className="h-5 text-right">{forgotText}</span>)}
     </div>
   )
 }
@@ -34,20 +34,20 @@ function HomePageTabs(){
         <Tab.Group>
           <Tab.List>
             {["Login", "Sign Up"].map((txt)=>(
-              <Tab className={`rounded-t-md ui-selected:bg-iso-white ui-not-selected:bg-iso-grey h-10 w-32`}>
+              <Tab className={"tab"}>
                 {txt}
               </Tab>
             ))}
           </Tab.List> 
           <Tab.Panels>
-            <Tab.Panel className="w-96 h-1/3 bg-iso-white p-10 rounded-b-md rounded-tr-md">
+            <Tab.Panel className="tab-body">
               <HomePageInput label="Username" forgot={true} forgotText={"Forgot Username"}/>
               <HomePageInput label="Password" hidden={true} forgot={true} forgotText={"Forgot Password"}/>
               <HomePageButton text="Login" OnClick={movePage}/>
             </Tab.Panel>
-            <Tab.Panel className="flex flex-col gap-4 w-96 h-1/3 bg-iso-white p-10 rounded-b-md rounded-tr-md">
+            <Tab.Panel className="flex flex-col tab-body gap-5">
               <HomePageInput label="Username" />
-              <HomePageInput label="Password" />
+              <HomePageInput label="Password" hidden={true}/>
               <HomePageButton text="Sign Up" OnClick={movePage}/>
             </Tab.Panel>
           </Tab.Panels>
@@ -58,10 +58,7 @@ function HomePageTabs(){
 }
 
 function HomePage() {
-  //TODO 1: reduce boilerplate html; too many divs so u would need to move some divs into components folder
-  //TODO 2: reduce boilerplate tailwind css; maybe add a tailwind class to index.css and use here
   //TODO 3: implement text rememberance when switching tabs
-  //TODO 4: tweak font sizes and standardize font
 
   return (
     <div className="flex flex-col w-screen h-screen bg-iso-blue">
