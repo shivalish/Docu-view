57739982aac3f60b2101a15b1fe57634020f2915
pghezzi <pghezzi@umass.edu>
2023-10-22 16:10:32 -0400

    Removed Unnecessary Imports

--- server/src/main/java/FWD_Development/DocuView/controllers/api/v1/FileShareV1.java
+++ server/src/main/java/FWD_Development/DocuView/controllers/api/v1/FileShareV1.java
@@ -1,31 +1,7 @@
 package FWD_Development.DocuView.controllers.api.v1;
 
-
-/* CUSTOM ADDED LIBS */
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.HashMap;
-/* CUSTOM ADDED LIBS */
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.dao.EmptyResultDataAccessException;
-
 // FILTERS
 //      file_creation
 //      document_type
