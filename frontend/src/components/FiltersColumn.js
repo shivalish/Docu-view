40ddc478bf5a858ed64ccf80e61501d49c5cb8a5
noonbles <atrac480@gmail.com>
2023-10-28 00:17:50 -0400

    minor code revamp + implemented contxt

--- frontend/src/components/FiltersColumn.js
+++ frontend/src/components/FiltersColumn.js
@@ -1,14 +1,16 @@
-import { React, useState } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { Switch } from "@headlessui/react";
 import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
+import { fetchContext } from "./TableContext";
 import classNames from "classnames";
 
-function ModifiedSwitch({ label }) {
+//enables the filter
+function ModifiedSwitch({ label, changer, cmd }) {
   const [open, setOpen] = useState(false);
   return (
     <Switch
       enabled={open}
-      onChange={setOpen}
+      onChange={()=>{setOpen(!open); changer(cmd);}}
       className="flex flex-row items-center gap-6"
     >
       <span className="text-iso-white font-bold">{label}</span>
@@ -23,39 +25,77 @@ function ModifiedSwitch({ label }) {
   );
 }
 
-function FiltersColumn() {
-  const [open, setOpen] = useState(false);
-
+//this is each row of the dropdown menu
+//cmd is /api/v1/...
+function FilterRow({ name, textboxes = false, setQuery }) {
   return (
-    <div className="flex flex-col bg-iso-blue h-full w-full text-iso-white p-4">
-      <div
-        onClick={() => {
-          setOpen(!open);
-        }}
-        className="flex flex-col"
-      >
-        <span className="flex flex-row w-full h-10 items-center text-lg">
-          FILTERS
-          <ChevronDoubleRightIcon
-            className={classNames("w-6 h-6", open && "rotate-90")}
-          />
-        </span>
-      </div>
-      {open && (
-        <div className="flex flex-col">
-          {[
-            "Project Type",
-            "Attachment Type",
-            "Auction Type",
-            "Resource Type",
-          ].map((name) => (
-            <div className="flex w-full h-16 items-center border-b-2 border-iso-white">
-              <ModifiedSwitch label={name} />
-            </div>
-          ))}
+    <div className="flex flex-col w-full py-2 items-center border-b-2 border-iso-white">
+      <ModifiedSwitch label={name} changer={setQuery}/>
+      {textboxes && (
+        <div className="flex flex-row gap-4 w-full text-black">
+          <input className="w-10 bg-iso-grey border-iso-blue-grey-100 border-2" />
+          <input className="w-10 bg-iso-grey border-iso-blue-grey-100 border-2" />
         </div>
       )}
     </div>
+  );
+}
+
+function Tester(){ //testing if useContext works
+  
+
+  const x = useContext(fetchContext)
+
+
+  useEffect(()=>{
+    console.log("query is changing!", x)
+  }, [])
+  return(
+    <div>
+      {x}
+    </div>
+  );
+}
+
+//dropdown menu
+function FiltersColumn() {
+  const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState("/api/v1/");
+
+
+  return (
+    <fetchContext.Provider value={query}>
+      <Tester/>
+      <div className="flex flex-col bg-iso-blue h-full w-full text-iso-white p-4">
+        <div
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className="flex flex-col"
+        >
+          <span className="flex flex-row w-full h-10 items-center text-lg">
+            FILTERS
+            <ChevronDoubleRightIcon
+              className={classNames("w-6 h-6", open && "rotate-90")}
+            />
+          </span>
+        </div>
+        {open && (
+          <div className="flex flex-col">
+            {[
+              {name: "Project Type", textboxes: false, setQuery: ()=>{setQuery("bruh")}},
+              {name: "Attachment Type", textboxes: false, setQuery: ()=>{setQuery("bruh2")}},
+              {name: "Auction Type", textboxes: true, setQuery: (date1, date2)=>{setQuery(`${date1, date2} bruh3`)}},
+              {name: "Resource Type", textboxes: true, setQuery: ()=>{setQuery("bruh4")}}
+            ].map((row) => (
+              <FilterRow 
+              {...row}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </fetchContext.Provider>
   );
 }
 
