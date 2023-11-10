92946eedf8bde6e99a4b927c653943a33442b314
pghezzi <100241979+pghezzi@users.noreply.github.com>
2023-11-10 17:06:04 -0500

    Frontend stage (#40)
    * new text remembrance
    
    * minor code revamp + implemented contxt
    
    * revamped filter code + moved components
    
    * popover bugfix
    
    * small changes to reduce redundant works
    
    * FilterTypes update
    
    * Update README.md
    Update to readme to fix commands to match new structure of project
    
    * code revamp + revised filters
    
    * overflow-scroll -> auto to remove x axis bar
    
    * Init
    
    * Textbox
    
    * global variable now object, filterTypes modifications for easier backend integration, modifications to checkbox
    
    * .
    
    * 3 new filter types
    
    * mainpage visual change + css changes + code cleanup + checkbox selection tags and rememberance
    
    * overflow fix + placeholder txt + font changes
    
    * multi-select combobox and textbox
    
    * bugfix
    
    * added multiselect on year
    
    ---------
    
    Co-authored-by: denn-afk <2253450682@qq.com>
    Co-authored-by: noonbles <105326922+noonbles@users.noreply.github.com>
    Co-authored-by: noonbles <atrac480@gmail.com>
    Co-authored-by: Adam Trac <atrac@umass.edu>
    Co-authored-by: ShiyuZhang <shiyuzhang2003@gmail.com>
    Co-authored-by: denn-afk <59733574+denn-afk@users.noreply.github.com>
    Co-authored-by: Pablo Ghezzi <pgsurf@kali.kali>
    Co-authored-by: sjakes201 <sjakes201@gmail.com>
    Co-authored-by: Jacob Sweet <92002279+sjakes201@users.noreply.github.com>

--- frontend/src/components/FiltersColumn.js
+++ frontend/src/components/FiltersColumn.js
@@ -1,61 +1,292 @@
-import { React, useState } from "react";
-import { Switch } from "@headlessui/react";
+import { React, useContext, useEffect, useState } from "react";
 import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
+import { fetchContext } from "./TableContext";
 import classNames from "classnames";
+import { Disclosure } from "@headlessui/react";
+import FilterTypes from "../atoms/FilterTypes";
+import { Combobox } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
+import Button from "../atoms/Button.jsx";
+import SelectionTag from "../atoms/SelectionTag.jsx";
+// import Calendar from 'react-calendar';
 
-function ModifiedSwitch({ label }) {
-  const [open, setOpen] = useState(false);
+//this is each row of the dropdown menu
+//textboxes = 1 or 2 dictactes how many textboxes there will be
+//setQuery is the function called (might have args)
+function FilterRow({
+  name,
+  textbox = false,
+  checkbox = [],
+  combo = [],
+  dropdown = [],
+  placeholder = "",
+  setQuery,
+  api,
+}) {
+  //textbox states
+  const [text, setText] = useState("");
+  const [textlog, setTextLog] = useState(new Set());
+
+  //combobox states
+  const [selectedCombo, setCombo] = useState(null);
+  const [comboText, setComboText] = useState("");
+  const [combolog, setComboLog] = useState(new Set());
+
+  //dropdown states
+  const [currYear, setYear] = useState(new Set());
+
+  //checkbox states
+  const [selectedCheck, setCheck] = useState([]);
+
+  const [vals, setVals] = useState(api);
+
+  //TODO: code cleanup + visual alignment fixes
+
+  //TODO: create a custom textbox component
+
+  const filtered =
+    comboText === ""
+      ? combo
+      : combo.filter((val) =>
+          val.toLowerCase().includes(comboText.toLowerCase())
+        );
+
+  //adds a key/value pair to the global query variable
+  const click = (selectedValue) => {
+    setVals({ [Object.keys(vals)[0]]: selectedValue });
+    setQuery(vals);
+  };
+
   return (
-    <Switch
-      enabled={open}
-      onChange={setOpen}
-      className="flex flex-row items-center gap-6"
-    >
-      <span className="text-iso-white font-bold">{label}</span>
-      <div className="flex h-5 w-10 bg-iso-blue-grey-200 rounded-full items-center">
-        <span
-          className={`${
-            open ? "translate-x-6" : "translate-x-0"
-          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-        />
-      </div>
-    </Switch>
-  );
-}
+    <div className="w-full">
+      {checkbox.length > 0 && (
+        <div>
+          <Disclosure className="w-full">
+            {({ open }) => (
+              <div>
+                <Disclosure.Button>
+                  <span className="flex flex-row w-full h-10 items-center text-lg pl-2">
+                    {name}
+                    <ChevronDoubleRightIcon
+                      className={classNames("w-6 h-6", open && "rotate-90")}
+                    />
+                  </span>
+                </Disclosure.Button>
+                <Disclosure.Panel>
+                  <div className="flex flex-col gap-2 w-full">
+                    {checkbox.map((docType, index) => (
+                      <div>
+                        <input
+                          type="checkbox"
+                          id={index}
+                          checked={
+                            selectedCheck.find((v) => v === docType) !==
+                            undefined
+                          }
+                          className="form-checkbox mx-1"
+                          onChange={(e) => {
+                            e.target.checked
+                              ? selectedCheck.push(docType)
+                              : selectedCheck.splice(
+                                  selectedCheck.indexOf(docType),
+                                  1
+                                );
+                            click(selectedCheck);
+                          }}
+                        />
+                        <label>{docType}</label>
+                      </div>
+                    ))}
+                  </div>
+                </Disclosure.Panel>
+              </div>
+            )}
+          </Disclosure>
+          {selectedCheck.length > 0 && (
+            <div>
+              {selectedCheck.map((e, index) => (
+                <SelectionTag
+                  key={index}
+                  value={e}
+                  onDelete={() => {
+                    selectedCheck.splice(selectedCheck.indexOf(e), 1);
+                    click(selectedCheck);
 
-function FiltersColumn() {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <div className="flex flex-col bg-iso-blue h-full w-full text-iso-white p-4">
-      <div
-        onClick={() => {
-          setOpen(!open);
-        }}
-        className="flex flex-col"
-      >
-        <span className="flex flex-row w-full h-10 items-center text-lg">
-          FILTERS
-          <ChevronDoubleRightIcon
-            className={classNames("w-6 h-6", open && "rotate-90")}
-          />
-        </span>
-      </div>
-      {open && (
-        <div className="flex flex-col">
-          {[
-            "Project Type",
-            "Attachment Type",
-            "Auction Type",
-            "Resource Type",
-          ].map((name) => (
-            <div className="flex w-full h-16 items-center border-b-2 border-iso-white">
-              <ModifiedSwitch label={name} />
+                    checkbox.forEach((box, i) => {
+                      if (document.getElementById(i)) {
+                        document.getElementById(i).checked =
+                          selectedCheck.find((v) => v === box) !== undefined;
+                      }
+                    });
+                  }}
+                />
+              ))}
             </div>
-          ))}
+          )}
+        </div>
+      )}
+      {dropdown.length > 0 && (
+        <div className="bg-iso-blue w-full">
+          <Menu>
+            {({ open }) => (
+              <div>
+                <Menu.Button className="w-full h-10">
+                  <span className="flex flex-row w-full h-10 items-center text-lg pl-2">
+                    {name}
+                    <ChevronDoubleRightIcon
+                      className={classNames("w-6 h-6", open && "rotate-90")}
+                    />
+                  </span>
+                </Menu.Button>
+                <Menu.Items className="h-32 overflow-auto">
+                  {dropdown.map((year) => (
+                    <Menu.Item
+                      onClick={() => {
+                        currYear.add(year);
+                        click(currYear);
+                      }}
+                    >
+                      <div className="w-full justify-center items-center hover:text-blue-400">
+                        <span className="cursor-pointer">{year}</span>
+                      </div>
+                    </Menu.Item> //TODO: overall style rework
+                  ))}
+                </Menu.Items>
+              </div>
+            )}
+          </Menu>
+          {currYear.size > 0 && (
+            [...currYear].map((e, i)=> (
+              <SelectionTag
+              value={e}
+              key={i}
+              onDelete={() => {
+                currYear.delete(e);
+                click(currYear);
+              }}
+            />
+            ))
+          )}
+        </div>
+      )}
+      {combo.length > 0 && (
+        <div className="w-full p-2">
+          <span className="text-lg"> {name} </span>
+          <Combobox value={selectedCombo} onChange={setCombo}>
+            <Combobox.Input
+              onChange={(e) => setComboText(e.target.value)}
+              className="textbox bg-iso-blue-grey-300 w-full max-w-full"
+              onKeyUp={e => {
+                if(e.key==='Enter' && selectedCombo !== null){
+                  combolog.add(selectedCombo);
+                  setComboText("");
+                  setCombo(null);
+                }}}
+              placeholder={placeholder}
+            />
+            <Combobox.Options className="flex flex-col pt-1 gap-1">
+              {filtered.map((val, index) => (
+                <Combobox.Option
+                  key={index}
+                  value={val}
+                  className="rounded-md ui-active:bg-iso-blue-grey-200
+                  ui-active:text-white bg-iso-blue-grey-300
+                  text-iso-white overflow-hidden text-sm p-1"
+                  onClick={() => combolog.add(val)}
+                >
+                  {val}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
+          {combolog.size > 0 && (
+            [...combolog].map((e,i) => (
+              <SelectionTag
+                key={i}
+                value={e}
+                onDelete={() => {
+                  combolog.delete(e);
+                  click(combolog);
+                }}
+              />
+            ))
+          )}
+        </div>
+      )}
+
+      {textbox && (
+        <div className="w-full p-2">
+          <label className="text-lg">{name}</label>
+          <input
+            className="textbox bg-iso-blue-grey-300"
+            onChange={e => setText(e.target.value)}
+            onKeyUp={e => {
+              if(e.key==='Enter'){
+                textlog.add(text)
+                setText("");
+              }}}
+            value={text}
+            placeholder={placeholder}
+          />
+          {textlog.size > 0 && (
+            [...textlog].map((e, i) => (
+              <SelectionTag
+                key={i}
+                value={e}
+                onDelete={() => {
+                  textlog.delete(e);
+                  click(textlog);
+                }}
+              />
+            ))
+          )}
         </div>
       )}
     </div>
+  );
+}
+
+//TODO: remove tester component when implementing table
+function Tester() {
+  //testing if useContext works
+
+  const x = useContext(fetchContext);
+
+  useEffect(() => {
+    console.log("query is changing!", x);
+  }, [x]);
+  return <div>{JSON.stringify(x)}</div>;
+}
+
+//dropdown menu
+function FiltersColumn() {
+  const [query, setQuery] = useState({});
+
+  return (
+    <fetchContext.Provider value={query}>
+      <div className="flex flex-col bg-iso-blue h-[50rem] w-full text-iso-white p-4 gap-2 overflow-y-scroll">
+          <div className="flex flex-col border-t">
+            {FilterTypes.map((row) => (
+              <div className={`py-2 border-b`}>
+                <FilterRow
+                  {...row}
+                  setQuery={(values) => setQuery({ ...query, ...values })}
+                />
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-center">
+            <Button
+              onClick={() => console.log("do something")}
+              height="h-full"
+              width="w-1/2"
+            >
+              Submit
+            </Button>
+            </div>
+      </div>
+    </fetchContext.Provider>
   );
 }
 
