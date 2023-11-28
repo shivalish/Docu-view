5c665a7425cd460cf97810dbcdf56bea02797874
pghezzi <pghezzi@umass.edu>
2023-11-28 13:24:44 -0500

    removed front end to avoid conflicts

--- frontend/src/atoms/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from '../imgs/logo.jpg';
-
-export default function Header(){
-    return (<div className="w-full h-auto bg-iso-white">
-        <img src={logo} alt="iso new england logo"className="h-full p-4 w-42"/>
-        <div className="w-full h-12 bg-iso-blue" />
-    </div>)
-}
--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Pages from "./Pages";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Pages />);
